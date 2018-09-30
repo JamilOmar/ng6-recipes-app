5aa9451416f76239955c2bf483692e23ea355772
Jamil Omar Falconi Aguirre <jamilomar.falconiaguirre@gmail.com>
2018-09-30 18:28:35 -0400

    feat: initial commit

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Tomatoes' , 13),
+    new Ingredient('Potatos' , 56)
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
